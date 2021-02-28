809644c497f7ff4c0139ca1a534f9b7757ec371d
Kristina <59376760+kristinarmontanez@users.noreply.github.com>
2021-02-27 19:00:41 -0800

    Update text.js

--- text.js
+++ text.js
@@ -1,21 +1,38 @@
+//---------------------------TEXT FILE---------------------------------------
+//---------------------------------------------------------------------------
+
+
+//-----------------All text for the Index page (main page):--------------------
+
+document.getElementById("I1").innerHTML = "Disclaimer & Fair Use Statement"
+
+
+
+//---------------------------All text for the About page: -----------------------
+
+
+//---------------------------All text for the Visit page: -----------------------
+
+
+
+//---------------------------All text for the Contact page: ---------------------
+
+
+//---------------------------Copyright Disclaimer: -------------------------------
 
 
 
 
-
-
-
-
-
-//Copyright Disclaimer. To be used as the footer of each page. 
-
-
-
+//To be used as the footer of each page. 
 document.getElementById("F1").innerHTML = "Disclaimer & Fair Use Statement"
-document.getElementById("F2").innerHTML = "This website may contain copyrighted material, the use of which may not have been specifically authorized by the copyright owner.\
-      This material is used to illustrate the use of website development tools as part of a class project and is strictly for educational purposes only.\
-      The material contained in this website is distributed without profit and cites all origination of material accordingly.\
-      This constitutes  ‘fair use’ of any such copyrighted material referenced and provided for in section 107 of the US Copyright Law.\
-      If you wish to use any copyrighted material from this site for purposes of your own that go beyond ‘fair use’,\
+
+document.getElementById("F2").innerHTML = "This website may contain copyrighted material,\
+      the use of which may not have been specifically authorized by the copyright owner.\
+      This material is used to illustrate the use of website development tools as part of\
+      a class project and is strictly for educational purposes only. The material contained\
+      in this website is distributed without profit and cites all origination of material \
+      accordingly.This constitutes  ‘fair use’ of any such copyrighted material referenced \
+      and provided for in section 107 of the US Copyright Law. If you wish to use any \
+      copyrighted material from this site for purposes of your own that go beyond ‘fair use’,\
       you must obtain expressed permission from the copyright owner."
 
