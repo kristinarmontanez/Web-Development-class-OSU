47484d6fc99d07d484aefb954a335b7cd66e2f91
Kristina <59376760+kristinarmontanez@users.noreply.github.com>
2021-02-28 14:12:54 -0800

    Update functions_and_text.js

--- functions_and_text.js
+++ functions_and_text.js
@@ -3,6 +3,29 @@
 
 
 
+
+//-----------------------------------------------Footer Text JavaScript ------------------------------------------------
+
+
+
+//To be used as the footer of each page. 
+document.getElementById("F1").innerHTML = "Disclaimer & Fair Use Statement"
+
+document.getElementById("F2").innerHTML = "This website may contain copyrighted material,\
+      the use of which may not have been specifically authorized by the copyright owner.\
+      This material is used to illustrate the use of website development tools as part of\
+      a class project and is strictly for educational purposes only. The material contained\
+      in this website is distributed without profit and cites all origination of material \
+      accordingly.This constitutes  ‘fair use’ of any such copyrighted material referenced \
+      and provided for in section 107 of the US Copyright Law. If you wish to use any \
+      copyrighted material from this site for purposes of your own that go beyond ‘fair use’,\
+      you must obtain expressed permission from the copyright owner."
+
+
+
+
+
+
 //-----------------------------------------------Carousel JavaScript --------------------------------------------------
 
 
@@ -169,22 +192,6 @@ scrollContentWrapper.addEventListener('scroll', moveScroller);
 
 
 
-//-----------------------------------------------Text JavaScript ----------------------------------------------------
-
-
-
-//To be used as the footer of each page. 
-document.getElementById("F1").innerHTML = "Disclaimer & Fair Use Statement"
-
-document.getElementById("F2").innerHTML = "This website may contain copyrighted material,\
-      the use of which may not have been specifically authorized by the copyright owner.\
-      This material is used to illustrate the use of website development tools as part of\
-      a class project and is strictly for educational purposes only. The material contained\
-      in this website is distributed without profit and cites all origination of material \
-      accordingly.This constitutes  ‘fair use’ of any such copyrighted material referenced \
-      and provided for in section 107 of the US Copyright Law. If you wish to use any \
-      copyrighted material from this site for purposes of your own that go beyond ‘fair use’,\
-      you must obtain expressed permission from the copyright owner."
 
 
 
