6335458963a02a324a22e4ef46e74fee311cb145
Kristina <59376760+kristinarmontanez@users.noreply.github.com>
2021-02-27 16:53:21 -0800

    Update text.js

--- text.js
+++ text.js
@@ -1,9 +1,18 @@
+
+document.getElementById("F1").innerHTML = "Hello Dolly!";
+
+
+
+
+
+
+
 //Copyright Disclaimer. To be used as the footer of each page. 
 
 
 
-F1 = "Disclaimer & Fair Use Statement"
-F2 = "This website may contain copyrighted material, the use of which may not have been specifically authorized by the copyright owner.\
+document.getElementById("F1").innerHTML = "Disclaimer & Fair Use Statement"
+document.getElementById("F2").innerHTML = "This website may contain copyrighted material, the use of which may not have been specifically authorized by the copyright owner.\
       This material is used to illustrate the use of website development tools as part of a class project and is strictly for educational purposes only.\
       The material contained in this website is distributed without profit and cites all origination of material accordingly.\
       This constitutes  ‘fair use’ of any such copyrighted material referenced and provided for in section 107 of the US Copyright Law.\
