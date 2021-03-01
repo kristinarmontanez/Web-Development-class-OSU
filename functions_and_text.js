607a9f10c989f25cc567e5da930e5ec132a46011
Kristina <59376760+kristinarmontanez@users.noreply.github.com>
2021-02-28 21:57:43 -0800

    Update functions_and_text.js

--- functions_and_text.js
+++ functions_and_text.js
@@ -13,11 +13,11 @@ document.getElementById("F1").innerHTML = "Disclaimer & Fair Use Statement"
 
 document.getElementById("F2").innerHTML = "This website may contain copyrighted material,\
       the use of which may not have been specifically authorized by the copyright owner.\
-      This material is used to illustrate the use of website development tools as part of\
+      This material is used to illustrate the use of website development as part of\
       a class project and is strictly for educational purposes only. The material contained\
       in this website is distributed without profit and cites all origination of material \
-      accordingly.This constitutes  ‘fair use’ of any such copyrighted material referenced \
-      and provided for in section 107 of the US Copyright Law. If you wish to use any \
+      accordingly. This constitutes ‘fair use’ of any such copyrighted material referenced \
+      and provided for in 17 U.S. Code § 107. If you wish to use any \
       copyrighted material from this site for purposes of your own that go beyond ‘fair use’,\
       you must obtain expressed permission from the copyright owner."
 
